c419a226e014675bbb75578ab0827c7419bea850
Jenniliu12 <93629514+Jenniliu12@users.noreply.github.com>
2025-11-29 19:35:42 +0100

    Update __init__.py

--- src/cytokine_dict/tl/__init__.py
+++ src/cytokine_dict/tl/__init__.py
@@ -1,5 +1,5 @@
 from .basic import basic_tool
 from .run_enrichment_test import run_enrichment_test
-from .check_robustness import check_robustness
+from .get_robust_significant_results import get_robust_significant_results
  
-__all__ = ["run_enrichment_test", "check_robustness"]
+__all__ = ["run_enrichment_test", "get_robust_significant_results"]
