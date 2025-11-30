f640ec05edd56452b7b0183412db44f088d4fff1
Jenniliu12 <93629514+Jenniliu12@users.noreply.github.com>
2025-11-30 14:00:49 +0100

    Update __init__.py

--- src/cytokine_dict/pl/__init__.py
+++ src/cytokine_dict/pl/__init__.py
@@ -1,4 +1,5 @@
 from .basic import BasicClass, basic_plot
 from .create_heatmap import create_heatmap
+from .heatmap_plot.py import plot_significant_results
  
-__all__ = ["create_heatmap"]
+__all__ = ["create_heatmap", "plot_significant_results"]
