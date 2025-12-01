910339b1d0f9f7252e4891d9c150e3f50264d40d
Jenniliu12 <93629514+Jenniliu12@users.noreply.github.com>
2025-12-01 01:33:34 +0100

    Update __init__.py

--- src/cytokine_dict/pl/__init__.py
+++ src/cytokine_dict/pl/__init__.py
@@ -1,6 +1,6 @@
 from .basic import BasicClass, basic_plot
 from .create_heatmap import create_heatmap
 from .heatmap_plot import plot_significant_results
-from .plot_communication import plot_communication
+from .plot_communication import plot_communication, get_senders_and_receivers
  
-__all__ = ["create_heatmap", "plot_significant_results", "plot_communication"]
+__all__ = ["create_heatmap", "plot_significant_results", "plot_communication", "get_senders_and_receivers"]
