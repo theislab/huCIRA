f468f3e195a699563d76ab2e5ceeb9cb1f89048a
Jenniliu12 <93629514+Jenniliu12@users.noreply.github.com>
2025-12-02 00:01:26 +0100

    Update __init__.py

--- src/cytokine_dict/tl/__init__.py
+++ src/cytokine_dict/tl/__init__.py
@@ -1,6 +1,6 @@
 from .basic import basic_tool
-from .enrichment_test import run_enrichment_test
+from .enrichment_test import run_one_enrichment_test, run_all_enrichment_test
 from .helper_functions import create_celltype_combos
 from .robustness_test import check_robustness, get_robust_significant_results
 
-__all__ = ["run_enrichment_test", "get_robust_significant_results", "check_robustness", "create_celltype_combos"]
+__all__ = ["run_one_enrichment_test", "run_all_enrichment_test", "get_robust_significant_results", "check_robustness", "create_celltype_combos"]
