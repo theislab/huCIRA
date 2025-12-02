8349c18085706b1b4b67c6f97d9406ae40451101
Jenniliu12 <93629514+Jenniliu12@users.noreply.github.com>
2025-12-02 01:14:28 +0100

    Update __init__.py

--- src/cytokine_dict/__init__.py
+++ src/cytokine_dict/__init__.py
@@ -4,7 +4,7 @@ from importlib.metadata import version
 
 from .data import load_cytokine_dict_data, load_MS_data
 from .tl import check_robustness, create_celltype_combos, get_robust_significant_results, run_one_enrichment_test, run_all_enrichment_test
-from .pl import create_heatmap, plot_significant_results, plot_communication, get_senders_and_receivers
+from .pl import create_heatmap, plot_significant_results, plot_communication, get_one_senders_and_receivers, get_all_senders_and_receivers
 
 __all__ = [
     "load_cytokine_dict_data",
@@ -18,7 +18,8 @@ __all__ = [
     "heatmap_plot",
     "plot_significant_results",
     "plot_communication",
-    "get_senders_and_receivers"
+    "get_one_senders_and_receivers",
+    "get_all_senders_and_receivers"
 ]
 
 
