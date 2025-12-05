396d8472a573df8d0ff99e2264cc2aa7ba4f0345
Jenniliu12 <93629514+Jenniliu12@users.noreply.github.com>
2025-12-05 01:31:00 +0100

    Update __init__.py

--- src/cytokine_dict/pl/__init__.py
+++ src/cytokine_dict/pl/__init__.py
@@ -1,4 +1,3 @@
-from .basic import BasicClass, basic_plot
 from .heatmap_plot import plot_significant_results
 from .plot_communication import plot_communication, get_one_senders_and_receivers, get_all_senders_and_receivers
  
