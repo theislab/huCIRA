6d3274c6fdc52a3948d4f6c5dfc4fe9c456b9433
Jenniliu12 <93629514+Jenniliu12@users.noreply.github.com>
2025-12-05 01:10:06 +0100

    Update __init__.py

--- src/cytokine_dict/__init__.py
+++ src/cytokine_dict/__init__.py
@@ -1,14 +1,15 @@
 from importlib.metadata import version
-# from . import pl, pp, tl
-# __all__ = ["pl", "pp", "tl"]
+# from . import pl, tl
+# __all__ = ["pl", "tl"]
 
-from .data import load_cytokine_dict_data, load_MS_data
+from .data import load_cytokine_dict_data, load_MS_data, load_cytokine_info
 from .tl import check_robustness, create_celltype_combos, get_robust_significant_results, run_one_enrichment_test, run_all_enrichment_test
 from .pl import plot_significant_results, plot_communication, get_one_senders_and_receivers, get_all_senders_and_receivers
 
 __all__ = [
     "load_cytokine_dict_data",
     "load_MS_data",
+    "load_cytokine_info",
     "check_robustness",
     "create_celltype_combos",
     "get_robust_significant_results",
