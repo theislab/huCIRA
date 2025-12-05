912d2927f5adcefad89ed4c6ffa8a2a0744259df
Jenniliu12 <93629514+Jenniliu12@users.noreply.github.com>
2025-12-05 01:08:42 +0100

    Update datasets.py
    added load_cytokine_info loader

--- src/cytokine_dict/data/datasets.py
+++ src/cytokine_dict/data/datasets.py
@@ -4,7 +4,7 @@ import scanpy as sc
 
 def load_cytokine_dict_data():
   """
-  To be changed. 
+  To be changed: Currently referring to local path.
   """
   cytokine_dict = pd.read_csv("/home/icb/jenni.liu/projects/cytokine_dict_folder/DEGs_all_with_pbs_DE_count.csv", index_col=0)
   return cytokine_dict
@@ -12,7 +12,17 @@ def load_cytokine_dict_data():
 
 def load_MS_data():
   """
-  To be changed. 
+  To be changed. Currently referring to local path.
   """
   adata = sc.read_h5ad("/home/icb/jenni.liu/projects/cytokine_dict_folder/Schafflick20_MS_CSF.h5ad")
   return adata
+
+
+def load_cytokine_info():
+    """
+    Cytokine information sheet includes information about sender and receptor genes (for cell-cell communication plot).
+    """
+
+    url = "https://github.com/theislab/cytokine_dict/edit/main/src/cytokine_dict/data/20250125_cytokine_info_with_functional_classification_LV.xlsx"
+    cytokine_info = pd.read_excel(url, sheet_name="all_cytokines")
+    return cytokine_info
