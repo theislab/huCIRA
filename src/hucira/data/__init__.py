7ad93460501c73614c641b8f8e753f8b6b09e48b
Jenniliu12 <93629514+Jenniliu12@users.noreply.github.com>
2025-12-16 22:57:49 +0100

    Update __init__.py

--- src/hucira/data/__init__.py
+++ src/hucira/data/__init__.py
@@ -1,3 +1,3 @@
-from .datasets import load_human_cytokine_dict, load_MS_CSF_data, load_Lupus_data, load_cytokine_info
+from .datasets import load_human_cytokine_dict, load_MS_CSF_data, load_Lupus_data, load_cytokine_info, load_CIP_signatures
 
-__all__ = ["load_human_cytokine_dict", "load_MS_CSF_data", "load_Lupus_data", "load_cytokine_info"]
+__all__ = ["load_human_cytokine_dict", "load_MS_CSF_data", "load_Lupus_data", "load_cytokine_info", "load_CIP_signatures"]
