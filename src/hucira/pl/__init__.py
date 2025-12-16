7a053c5b904fbda148a0573636dac71d68934f62
Jenniliu12 <93629514+Jenniliu12@users.noreply.github.com>
2025-12-16 22:51:11 +0100

    Update __init__.py

--- src/hucira/pl/__init__.py
+++ src/hucira/pl/__init__.py
@@ -1,9 +1,7 @@
-from .plot_communication import get_all_senders_and_receivers, get_one_senders_and_receivers, plot_communication
+from .plot_communication import plot_communication
 from .plot_significant_results import plot_significant_results
 
 __all__ = [
     "plot_significant_results",
-    "plot_communication",
-    "get_one_senders_and_receivers",
-    "get_all_senders_and_receivers",
+    "plot_communication"
 ]
