328ecee4c4ae537d2d2c32352a5bc0955ae32b6c
Jenniliu12 <93629514+Jenniliu12@users.noreply.github.com>
2025-12-16 22:50:19 +0100

    Update __init__.py

--- src/hucira/tl/__init__.py
+++ src/hucira/tl/__init__.py
@@ -1,8 +1,12 @@
+from .cytokine_communication import get_one_senders_and_receivers, get_all_senders_and_receivers
 from .enrichment_test import run_all_enrichment_test, run_one_enrichment_test
 from .helper_functions import create_celltype_combos
 from .robustness_test import check_robustness, get_robust_significant_results
 
+
 __all__ = [
+    "get_one_senders_and_receivers",
+    "get_all_senders_and_receivers",
     "run_one_enrichment_test",
     "run_all_enrichment_test",
     "get_robust_significant_results",
